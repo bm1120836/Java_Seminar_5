7e960b778d721ea9b2f5f3643eaff255535937dd
BM <bm1120836@yandex.ru>
2022-12-30 22:26:10 +0300

    подсчитать количество вхождений каждого слова

--- Task1.java
+++ Task1.java
@@ -9,32 +9,28 @@ import java.util.Map;
 
  class Task1 {
     public static void main(String[] args) {
-        String text = "Россия идет вперед всей планеты. Планета — это не Россия.";
-        text = text.replaceAll("[-|–|—]|\\p{Punct}", " ");
-        while (text.contains("  ")){
-            text = text.replace("  ", " ");
-        }
-        String findWord = "Планета";
-        String[] arrayText = text.split(" ");
-        Map<String, Integer> map = new HashMap<>();
-        for (String currentWord : arrayText){
-            if (!findWord.toLowerCase().equals(currentWord.toLowerCase())){
-                continue;
+            String text = "Россия идет вперед всей планеты. Планета — это не Россия.";
+            text = text.replaceAll("[-|–|—]|\\p{Punct}", " ");
+            while (text.contains("  ")){
+                text = text.replace("  ", " ");
             }
-            int count = map.getOrDefault(findWord.toLowerCase(), 0);
-            map.put(currentWord.toLowerCase(), ++count);
+            String[] arrayText = text.split(" ");
+    
+            Map<String, Integer> map = getCountWords(arrayText);
+    
+            for(Map.Entry<String, Integer> entry : map.entrySet()){
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+            }
         }
-
-        System.out.println("Количество искомого слова: " + map.getOrDefault(findWord.toLowerCase(), 0));
-
+    
+        public static Map<String, Integer> getCountWords(String[] array){
+            Map<String, Integer> map = new HashMap<>();
+    
+            for (String current : array){
+                int count = map.getOrDefault(current.toLowerCase(), 0);
+                map.put(current.toLowerCase(), ++count);
+            }
+    
+            return map;
+        }
     }
-
-
-
-
-
-
-
-
-
- }
